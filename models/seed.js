7fb4a6e39b3e3d91a632b0ff7335be15b1dee6ee
Rick Christenhusz <nibblet787@gmail.com>
2018-11-26 14:22:12 -0500

    populated seed file

---
+++ models/seed.js
@@ -0,0 +1,41 @@
+module.exports = [
+  {
+  host: false,
+  hostName: 'South America',
+  hostDate: ,
+  name: 'Jim Jones',
+  food: 'Kool-aid',
+  recipeUrl: 'https://goo.gl/gjxsfn',
+  profilePic: 'https://goo.gl/YycNWy',
+  needOven: false,
+  attendees: 300,
+  glutenFree: false,
+  peanutFree: true
+  },
+  {
+  host: true,
+  hostName: ,
+  hostDate: ,
+  name: 'Marie Antoinette',
+  food: 'Peasant Cake',
+  recipeUrl: 'https://goo.gl/Jo61hS',
+  profilePic: 'https://goo.gl/a2ntSc',
+  needOven: true,
+  attendees: 5407,
+  glutenFree: false,
+  peanutFree: true
+  },
+  {
+  host: false,
+  hostName: 'Athens',
+  hostDate: ,
+  name: 'Socrates',
+  food: 'Hemlock',
+  recipeUrl: 'https://goo.gl/2l6fa',
+  profilePic: 'https://goo.gl/65yP9o',
+  needOven: false,
+  attendees: 1,
+  glutenFree: true,
+  peanutFree: true
+  },
+];
